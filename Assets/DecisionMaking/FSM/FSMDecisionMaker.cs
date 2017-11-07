70fee76af41861888c3da409ab89ced522c17516
CabbageWarrior <feelthecabbage@gmail.com>
2017-11-07 12:04:31 +0100

    Added FSM logic.

---
+++ Assets/DecisionMaking/FSM/FSMDecisionMaker.cs
@@ -0,0 +1,79 @@
+﻿using AI.Movement;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace AI.FSM
+{
+    public class FSMDecisionMaker : DecisionMaker
+    {
+        public Animator aiAnimator;
+
+        Agent agent;
+
+        // Use this for initialization
+        void Start()
+        {
+            agent = GetComponent<Agent>();
+
+            var seekBe = agent.GetComponent<SeekBehaviour>();
+            var fleeBe = agent.GetComponent<FleeBehaviour>();
+            var shootAction = agent.GetComponent<ShootAction>();
+
+            var goToBaseState = aiAnimator.GetBehaviour<GoToBaseState>();
+            goToBaseState.agent = agent;
+            goToBaseState.seekBe = seekBe;
+            goToBaseState.fleeBe = fleeBe;
+
+            var attackEnemyState = aiAnimator.GetBehaviour<AttackEnemyState>();
+            attackEnemyState.agent = agent;
+            attackEnemyState.seekBe = seekBe;
+            attackEnemyState.fleeBe = fleeBe;
+            attackEnemyState.shootAction = shootAction;
+
+            var getHealthState = aiAnimator.GetBehaviour<GetHealthState>();
+            getHealthState.agent = agent;
+            getHealthState.seekBe = seekBe;
+            getHealthState.fleeBe = fleeBe;
+
+            var RunAwayState = aiAnimator.GetBehaviour<RunAwayState>();
+            RunAwayState.agent = agent;
+            RunAwayState.seekBe = seekBe;
+            RunAwayState.fleeBe = fleeBe;
+
+            // Animator runs before Update, so we do the first check now
+            MakeDecision();
+        }
+
+        public override void MakeDecision()
+        {
+            // Check State Switching
+            // ... We just need to update inputs to the AC
+
+            float closestDistanceSqr = float.MaxValue;
+
+            var allAgents = FindObjectsOfType<Agent>();
+            foreach (var otherAgent in allAgents)
+            {
+                if (otherAgent != agent)
+                {
+                    if (
+                        (otherAgent.transform.position - transform.position).sqrMagnitude < closestDistanceSqr &&
+                        otherAgent.GetComponent<HealthState>().team != GetComponent<HealthState>().team
+                        )
+                    {
+                        closestDistanceSqr = (otherAgent.transform.position - transform.position).sqrMagnitude;
+                    }
+                }
+            }
+            aiAnimator.SetFloat("ClosestEnemyDistance", closestDistanceSqr);
+
+            aiAnimator.SetInteger("MyHealth", (int)GetComponent<HealthState>().health);
+
+            aiAnimator.SetInteger("HealthPickups", FindObjectsOfType<HealthPickup>().Length);
+
+            // Execute current state
+            // ... The AC will do that for us
+        }
+    }
+}
--- Assets/DecisionMaking/ShootAction.cs
+++ Assets/DecisionMaking/ShootAction.cs
@@ -1,15 +1,27 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class ShootAction : MonoBehaviour
 {
     public GameObject bulletPrefab;
+    public float shootRate = 1.0f;
     
     public void Shoot()
     {
         var bulletGo = Instantiate(bulletPrefab);
         bulletGo.transform.position = transform.position + transform.right * .25f;
         bulletGo.transform.rotation = transform.rotation;
+    }
+
+    internal void StartShooting()
+    {
+        InvokeRepeating("Shoot", shootRate, shootRate);
+    }
+
+    internal void StopShooting()
+    {
+        CancelInvoke("Shoot");
     }
 }
